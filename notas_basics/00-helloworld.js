28b25b616d7f8e437fef6132b18a9ac0934ccff7
IvTole <toledano.ivan16@gmail.com>
2025-06-12 23:39:27 -0600

    add topic 00

---
+++ notas_basics/00-helloworld.js
@@ -0,0 +1,25 @@
+// This is a comment
+/*
+This is a comment
+for several lines
+*/
+// el objeto console, log para interactuar con la terminal
+// Antes JS necesitaba ;, pero ahora ya no es tan necesario
+// Usamos el entorno de ejecucion de node
+// vemos como representar strings con tres tipos de comillas
+
+console.log("Hello Javascript")
+console.log('Hello Javascript')
+console.log(`Hello Javascript`)
+
+// podemos poner diferentes tipos de datos
+console.log(5)
+console.log("5")
+
+// operaciones básicas
+console.log(5 + 2)
+console.log(5 - 2)
+console.log(5 * 2)
+console.log(5 / 2)
+console.log(5 % 2)
+console.log(5 ** 2)
